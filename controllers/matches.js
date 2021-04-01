92fe45fd06b43d7ef77d8b855f477d38b6468901
unknown <premteastallstudio@gmail.com>
2021-04-01 18:05:59 +0530

    Add new file with socket

--- controllers/matches.js
+++ controllers/matches.js
@@ -1,4 +1,4 @@
-
+var db = require('../config/DbConnection');
 exports.listing = function (req, res, next) {
 
     try {
@@ -382,3 +382,225 @@ exports.UserMatchOdds = function (req, res, next) {
         }
     });
 }
+
+
+
+    exports.UserMatchOddstest =function(id,callback){
+
+        let internalData = {
+            eventTypeId: '',
+            matchType: '',
+            marketIDs: '',
+            homeTeamName: '',
+            awayTeamName: '',
+            score: [],
+            markets: [],
+            sessions: [],
+            stakes: [],
+        };
+    
+        async.series([
+    
+            function (do_callback) {
+    
+                // get markets & odds as per market
+                let query5 = `SELECT Market.id market_id, Market.marketId, Market.event_id, Market.marketName,
+                    MarketOdd.id market_odd_id, MarketOdd.status, MarketOdd.inplay
+                    FROM markets Market 
+                    JOIN market_odds MarketOdd ON MarketOdd.marketId = Market.marketId
+                    WHERE Market.event_id=${id}`;
+    
+                db.query(query5, function (err, results, fields) {
+    
+                    if (err) {
+                        do_callback(err);
+                    } else {
+    
+                        internalData.marketsAndMarketOdds = results;
+                        do_callback();
+                    }
+                });
+            },
+            function (do_callback) {
+    
+                let marketIDs = [];
+                internalData.marketsAndMarketOdds.forEach(element => {
+                    marketIDs.push(element.marketId);
+                });
+    
+                internalData.marketIDs = marketIDs.join();
+                do_callback();
+            },
+            function (do_callback) {
+    
+                // get markets & odds as per market
+                let query4 = `SELECT id, marketId, selectionId, runnerName FROM market_runners WHERE marketId IN (${internalData.marketIDs})`;
+    
+                db.query(query4, function (err, results, fields) {
+    
+                    if (err) {
+                        do_callback(err);
+                    } else {
+    
+                        internalData.market_runners = results;
+                        do_callback();
+                    }
+                });
+            },
+            function (do_callback) {
+    
+                // get market runners & odds as per runner
+                let query3 = `SELECT * FROM market_odd_runners WHERE marketId IN (${internalData.marketIDs})`;
+    
+                db.query(query3, function (err, results, fields) {
+    
+                    if (err) {
+                        do_callback(err);
+                    } else {
+    
+                        internalData.market_odd_runners = results;
+                        do_callback();
+                    }
+                });
+            },
+            function (do_callback) {
+    
+                // get team name
+                if(internalData.market_runners.length > 0) {
+                    internalData.homeTeamName = internalData.market_runners[0].runnerName;
+                    internalData.awayTeamName = internalData.market_runners[1].runnerName;
+                }
+    
+                let markets = [];
+                let singleRow = {};
+                let innerCount = -1;
+                async.forEach(internalData.marketsAndMarketOdds, (singleMarketsAndMarketOdds) => {
+    
+                    // add market and market odds
+                    singleRow = {
+                        event_id: singleMarketsAndMarketOdds.event_id,
+                        marketId: singleMarketsAndMarketOdds.marketId,
+                        marketName: singleMarketsAndMarketOdds.marketName,
+                        marketOddStatus: singleMarketsAndMarketOdds.status,
+                        inplay: singleMarketsAndMarketOdds.inplay,
+                        runners: [],
+                    };
+    
+                    internalData.market_runners.forEach(singleMarketRunner => {
+    
+                        if(singleMarketRunner.marketId == singleMarketsAndMarketOdds.marketId) {
+    
+                            innerCount++;
+    
+                            // get market runners odd 
+                            let singleMarketOddRunner = internalData.market_odd_runners[innerCount];
+    
+                            // add market runners 
+                            let data = {
+                                market_runner_id: singleMarketRunner.id,
+                                selectionId: singleMarketRunner.selectionId,
+                                runnerName: singleMarketRunner.runnerName,
+                                prices: [
+                                    {
+                                        price: singleMarketOddRunner.availableToBack_price_3,
+                                        size: singleMarketOddRunner.availableToBack_size_3,
+                                    },
+                                    {
+                                        price: singleMarketOddRunner.availableToBack_price_2,
+                                        size: singleMarketOddRunner.availableToBack_size_2,
+                                    },
+                                    {
+                                        price: singleMarketOddRunner.availableToBack_price_1,
+                                        size: singleMarketOddRunner.availableToBack_size_1,
+                                    },
+                                    {
+                                        price: singleMarketOddRunner.availableToLay_price_1,
+                                        size: singleMarketOddRunner.availableToLay_size_1,
+                                    },
+                                    {
+                                        price: singleMarketOddRunner.availableToLay_price_2,
+                                        size: singleMarketOddRunner.availableToLay_size_2,
+                                    },
+                                    {
+                                        price: singleMarketOddRunner.availableToLay_price_3,
+                                        size: singleMarketOddRunner.availableToLay_size_3,
+                                    }
+                                ]
+                            }
+                            singleRow.runners.push(data)
+                        }
+                    });
+    
+                    markets.push(singleRow);
+    
+                });
+    
+                internalData.markets = markets;
+                do_callback();
+    
+            },
+            function (do_callback) {
+    
+                // get sessions 
+                let query2 = `SELECT id session_id, event_id, RunnerName, LayPrice1, LaySize1, BackPrice1, BackSize1, GameStatus, SelectionId
+                    FROM match_sessions 
+                    WHERE event_id=${id} AND is_allowed=1 AND is_result=0 AND is_abandoned=0`;
+    
+                db.query(query2, function (err, results, fields) {
+    
+                    if (err) {
+                        do_callback(err);
+                    } else {
+    
+                        internalData.sessions = results;
+                        do_callback();
+                    }
+                });
+            },
+            function (do_callback) {
+    
+                // get score
+                let query1 = `SELECT * FROM match_scores WHERE event_id=${id}`;
+    
+                db.query(query1, function (err, results, fields) {
+    
+                    if (err) {
+                        do_callback(err);
+                    }
+                    else if (results.length > 0) {
+                        let score = JSON.parse(results[0].score);
+                        score.home.name = internalData.homeTeamName;
+                        score.away.name = internalData.awayTeamName;
+                        internalData.score = score;
+    
+                        internalData.eventTypeId = results[0].eventTypeId;
+                        internalData.matchType = (results[0].matchType) ? 'LIMITED_OVER' : null;
+                        do_callback();
+                    }
+                    else {
+                        do_callback();
+                    }
+                });
+            },
+        ], function (err) {
+            if (err) {
+                callback(err,null);
+            } else {
+    
+                let result = {
+                    status: true,
+                    message: 'User Match Odds',
+                    eventTypeId: internalData.eventTypeId,
+                    matchType: internalData.matchType,
+                    score: internalData.score,
+                    markets: internalData.markets,
+                    sessions: internalData.sessions,
+                }
+                callback(result,null);
+            }
+        });
+
+
+
+    }
+
